dbe61c25961932e4c3ffe15361e37d4e68fc4f55
Simon Tucker <simon@brotherlogic@gmail>
2020-01-12 17:33:52 -0800

    Adds config in to state report

--- githubtasks.go
+++ githubtasks.go
@@ -84,7 +84,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
 	return []*pbg.State{
-		&pbg.State{Key: "no", Value: int64(233)},
+		&pbg.State{Key: "config", Text: fmt.Sprintf("%v", s.config)},
 	}
 }
 
